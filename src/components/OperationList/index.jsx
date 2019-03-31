f0fe7a406a2cb35ca1f5a1c5ec075526ae2737e9
greshilov.v <greshilov@a2design.biz>
2019-03-31 17:05:44 +0600

    added: css for operations list

--- src/components/OperationList/index.jsx
+++ src/components/OperationList/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './OperationList.css';
+
 import OperationListItem from '../OperationListItem';
 
 const OperationList = props => {
   const { operations } = props;
 
   return (
-    <div>
+    <div className="operation-list">
       {operations.length ? (
         operations.map((element, index) => (
           <OperationListItem key={index} {...element} />
