c1784fac8e6397b0ed0a795b0c43212b81e1ff8c
greshilov.v <greshilov@a2design.biz>
2019-04-01 02:06:59 +0600

    Updated: OperationForm - added validation rules and messages - added initialValues

--- src/components/OperationModalForm/OperationForm.jsx
+++ src/components/OperationModalForm/OperationForm.jsx
@@ -8,50 +8,80 @@ import { operationName } from '../../helpers/functions';
 const OperationForm = ({ onCreateOperation }) => {
   const options = [c.OPERATION_CREDIT, c.OPERATION_DEBIT];
 
+  const initialValues = {
+    type: c.OPERATION_CREDIT,
+    amount: '',
+    description: null,
+  };
+
   const onSubmit = (values, form) => {
     onCreateOperation({
-      ...values,
+      type: values.type,
+      amount: Number(values.amount),
+      description: values.description,
       timestamp: new Date().getTime(),
     });
     form.reset();
   };
 
+  const required = value => (value ? undefined : 'Required');
+  const mustBeNumber = value => (isNaN(value) ? 'Must be a number' : undefined);
+  const minValue = min => value =>
+    isNaN(value) || value >= min ? undefined : `Should be greater than ${min}`;
+  const composeValidators = (...validators) => value =>
+    validators.reduce(
+      (error, validator) => error || validator(value),
+      undefined
+    );
+
   return (
     <Form
       onSubmit={onSubmit}
+      initialValues={initialValues}
       render={({ handleSubmit, form, submitting, pristine, values }) => (
         <form className="operation-form" onSubmit={handleSubmit}>
-          <div className="operation-form_row">
-            <label htmlFor="type">Type of operation</label>
-            <Field id="type" name="type" component="select">
-              {options.map((option, key) => (
-                <option key={key} value={option}>
-                  {operationName(option)}
-                </option>
-              ))}
-            </Field>
-          </div>
+          <Field id="type" name="type" component="select" validate={required}>
+            {({ input, meta }) => (
+              <div className="operation-form_row">
+                <label htmlFor="type">Тип операции</label>
+                <select {...input} id="type">
+                  {options.map((option, key) => (
+                    <option key={key} value={option}>
+                      {operationName(option)}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
+          </Field>
 
-          <div className="operation-form_row">
-            <label htmlFor="amount">amount</label>
-            <Field
-              id="amount"
-              name="amount"
-              component="input"
-              type="text"
-              placeholer="amount"
-            />
-          </div>
+          <Field
+            name="amount"
+            validate={composeValidators(required, mustBeNumber, minValue(0))}>
+            {({ input, meta }) => (
+              <div className="operation-form_row">
+                <label htmlFor="amount">Сумма</label>
+                <input {...input} id="amount" type="text" placeholder="Сумма" />
+                {meta.error && meta.touched && (
+                  <span className="error">{meta.error}</span>
+                )}
+              </div>
+            )}
+          </Field>
 
-          <div className="operation-form_row">
-            <label htmlFor="description">description</label>
-            <Field
-              id="description"
-              name="description"
-              component="textarea"
-              placeholer="description"
-            />
-          </div>
+          <Field
+            id="description"
+            name="description"
+            component="textarea"
+            placeholer="description">
+            {({ input, meta }) => (
+              <div className="operation-form_row">
+                <label htmlFor="description">Описание</label>
+                <textarea {...input} id="description" cols="30" rows="10" />
+              </div>
+            )}
+          </Field>
+
           <div className="operation-form_row">
             <div className="buttons">
               <button type="submit" disabled={submitting || pristine}>
