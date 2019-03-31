4e5b0595dfbd4c9da16dd4e8fec0e01e8d7516f3
greshilov.v <greshilov@a2design.biz>
2019-03-31 19:27:16 +0600

    refactored: OperationForm - added onSubmit func - added div's separation

--- src/components/OperationModalForm/OperationForm.jsx
+++ src/components/OperationModalForm/OperationForm.jsx
@@ -5,52 +5,62 @@ import { Form, Field } from 'react-final-form';
 import * as c from '../../helpers/constants';
 import { operationName } from '../../helpers/functions';
 
-const OperationForm = ({ onSubmit }) => {
+const OperationForm = ({ onCreateOperation }) => {
   const options = [c.OPERATION_CREDIT, c.OPERATION_DEBIT];
+
+  const onSubmit = values =>
+    onCreateOperation({
+      ...values,
+    });
 
   return (
     <Form
       onSubmit={onSubmit}
       render={({ handleSubmit, form, submitting, pristine, values }) => (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="type">Type of operation</label>
-          <Field name="type" component="select">
-            <option />
-            {options.map((option, key) => (
-              <option key={key} value={option}>
-                {operationName(option)}
-              </option>
-            ))}
-          </Field>
+        <form className="operation-form" onSubmit={handleSubmit}>
+          <div className="operation-form_row">
+            <label htmlFor="type">Type of operation</label>
+            <Field id="type" name="type" component="select">
+              {options.map((option, key) => (
+                <option key={key} value={option}>
+                  {operationName(option)}
+                </option>
+              ))}
+            </Field>
+          </div>
 
-          <label htmlFor="amount">amount</label>
-          <Field
-            name="amount"
-            component="input"
-            type="text"
-            placeholer="amount"
-          />
+          <div className="operation-form_row">
+            <label htmlFor="amount">amount</label>
+            <Field
+              id="amount"
+              name="amount"
+              component="input"
+              type="text"
+              placeholer="amount"
+            />
+          </div>
 
-          <label htmlFor="description">description</label>
-          <Field
-            name="description"
-            component="textarea"
-            placeholer="description"
-          />
-
-          <div className="buttons">
-            <button type="submit" disabled={submitting || pristine}>
-              Submit
-            </button>
-            <button
-              type="button"
-              onClick={form.reset}
-              disabled={submitting || pristine}>
-              Reset
-            </button>
-            <button type="button" onClick={form.reset}>
-              close
-            </button>
+          <div className="operation-form_row">
+            <label htmlFor="description">description</label>
+            <Field
+              id="description"
+              name="description"
+              component="textarea"
+              placeholer="description"
+            />
+          </div>
+          <div className="operation-form_row">
+            <div className="buttons">
+              <button type="submit" disabled={submitting || pristine}>
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}>
+                Reset
+              </button>
+            </div>
           </div>
         </form>
       )}
