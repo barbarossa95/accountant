9ce018f09009887f407e38dbbbc2ac6f8c7fe607
greshilov.v <greshilov@a2design.biz>
2019-03-31 17:07:42 +0600

    added: OperationModalForm comp

---
+++ src/components/OperationModalForm/index.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+import ModalWindow from './ModalWindow';
+
+import './OperationModalForm.css';
+
+const OperationModalForm = ({ onSubmitHandler }) => {
+  const [isOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => setModalIsOpen(true);
+
+  const closeModal = () => setModalIsOpen(false);
+
+  return (
+    <React.Fragment>
+      <h2>Добавить новую операцию</h2>
+      <button onClick={openModal}>Добавить</button>
+
+      <ModalWindow isOpen={isOpen} closeModal={closeModal} />
+    </React.Fragment>
+  );
+};
+
+export default OperationModalForm;
