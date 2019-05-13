70ca4130df490515da33ce05570f276a5dbb5833
Vladimir Greshilov <greshilov@a2design.biz>
2019-05-13 11:39:36 +0600

    updated: operation api router

--- server/routes/api/operation.js
+++ server/routes/api/operation.js
@@ -1,23 +1,24 @@
-const express = require("express"),
+const express = require('express'),
   router = express.Router(),
-  Operation = require("../../models/Operation"),
-  operationManager = new Operation();
+  operationManager = require('../../models/Operation');
 
 router
-  .get("/", function(req, res) {
+  .get('/', function(req, res) {
     try {
       operationManager.list().then(data => res.json(data));
     } catch (error) {
       res.sendStatus(500);
     }
   })
-  .post("/", function(req, res) {
-    const data = req.body,
-      key = operationManager.create(data);
+  .post('/', function(req, res) {
+    const data = req.body;
 
-    res.json({ ...data, id: key });
+    operationManager
+      .create(data)
+      .then(operation => res.json({ operation }))
+      .catch(e => console.error(e));
   })
-  .delete("/:id", function(req, res) {
+  .delete('/:id', function(req, res) {
     try {
       const id = req.params.id;
 
