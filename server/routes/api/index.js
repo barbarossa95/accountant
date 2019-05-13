28ad65753ec5fb575d424fea68d3d73c38183126
Vladimir Greshilov <greshilov@a2design.biz>
2019-05-13 19:20:13 +0600

    refactored: reoutes/index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var operationRouter = require("./operation");
+const operationRouter = require('./operation');
+const authRouter = require('./auth');
 
-router.use("/operation", operationRouter);
+router.use('/operation', operationRouter);
+router.use('/auth', authRouter);
 
 module.exports = router;
