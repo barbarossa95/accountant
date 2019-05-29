4d7f19f87beedc5957db51c36bc08686376e3684
greshilov.v <greshilov@a2design.biz>
2019-05-29 23:32:33 +0600

    updated: operations api router - added jwt check and should auth middlewares - updated catch section of response promise

--- server/routes/api/operation.js
+++ server/routes/api/operation.js
@@ -1,14 +1,19 @@
-const express = require('express'),
+const { parseJwt, shouldAuth } = require('../../middleware'),
+  express = require('express'),
   router = express.Router(),
   Operation = require('../../repositories/OperationRepo'),
   operationRepo = new Operation();
 
 router
+  .use(parseJwt, shouldAuth)
   .get('/', function(req, res) {
     operationRepo
       .get()
       .then(data => res.json(data))
-      .catch(e => console.error(e));
+      .catch(e => {
+        console.error(e);
+        res.sendStatus(500);
+      });
   })
   .post('/', function(req, res) {
     const data = req.body;
@@ -16,16 +21,20 @@ router
     operationRepo
       .create(data)
       .then(operation => res.json({ operation }))
-      .catch(e => console.error(e));
+      .catch(e => {
+        console.error(e);
+        res.sendStatus(500);
+      });
   })
   .delete('/:key', function(req, res) {
-    try {
-      const { key = null } = req.params;
-
-      operationRepo.remove(key).then(() => res.sendStatus(204));
-    } catch (error) {
-      e => console.error(e);
-    }
+    const { key = null } = req.params;
+    operationRepo
+      .remove(key)
+      .then(() => res.sendStatus(204))
+      .catch(e => {
+        console.error(e);
+        res.sendStatus(500);
+      });
   });
 
 module.exports = router;
