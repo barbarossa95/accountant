72bc9deab637c6726b2bf799ce91ac3460a7a6c4
greshilov.v <greshilov@a2design.biz>
2019-05-30 13:46:15 +0600

    updated: user store action types

--- src/store/actionTypes/user.js
+++ src/store/actionTypes/user.js
@@ -1,3 +1,7 @@
 export const AUTH_FAIL = 'AUTH_FAIL';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+
+export const LOGOUT = 'LOGOUT';
