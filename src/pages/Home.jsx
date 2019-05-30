008b3b222050cdb19568298253cdbeef73541a08
greshilov.v <greshilov@a2design.biz>
2019-05-30 13:50:01 +0600

    removed: link from Home page comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../containers/Header';
 import TimeTable from '../containers/TimeTable';
 
 import { requireAuth } from '../hoc/requireAuth';
 const Home = () => (
   <React.Fragment>
-    <Link to="/login">login</Link>
     <Header />
     <TimeTable />
   </React.Fragment>
