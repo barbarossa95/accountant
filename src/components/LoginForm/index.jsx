3d7ef09b2d7c07cf61c8243f77459ffc94c7b1f0
greshilov.v <greshilov@a2design.biz>
2019-05-30 13:51:13 +0600

    added: loader on login page

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 import { Form, Field } from 'react-final-form';
 import Button from '../UI/Button';
+import Loader from '../UI/Loader';
 
 import { required } from '../../helpers/validation';
 
-const LoginForm = ({ login: dispatchLogin, loginMessage }) => {
+const LoginForm = ({ login: dispatchLogin, loginMessage, loginLoading }) => {
   const onSubmit = ({ username, password }, form) => {
       dispatchLogin(username, password)
         .then(() => form.reset())
@@ -18,70 +19,73 @@ const LoginForm = ({ login: dispatchLogin, loginMessage }) => {
     };
 
   return (
-    <Form
-      onSubmit={onSubmit}
-      initialValues={initialValues}
-      render={({ handleSubmit, form, values, submitting, pristine }) => (
-        <form className="login-form" onSubmit={handleSubmit}>
-          <div className="login-form_row">
-            <h2>Вход:</h2>
-          </div>
-          <div className="login-form_row">
-            <Field name="username" component="input" validate={required}>
-              {({ input }) => (
-                <label>
-                  Логин:
-                  <input
-                    {...input}
-                    id="login"
-                    tabIndex="1"
-                    placeholder="логин"
-                  />
-                </label>
-              )}
-            </Field>
-          </div>
-          <div className="login-form_row">
-            <Field name="password" component="input" validate={required}>
-              {({ input, meta }) => (
-                <label>
-                  Пароль:
-                  <input
-                    {...input}
-                    id="password"
-                    type="password"
-                    placeholder="пароль"
-                    tabIndex="2"
-                  />
-                </label>
-              )}
-            </Field>
-          </div>
+    <React.Fragment>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        render={({ handleSubmit, form, values, submitting, pristine }) => (
+          <form className="login-form" onSubmit={handleSubmit}>
+            {loginLoading ? <Loader className="overlay" /> : null}
 
-          {loginMessage ? (
             <div className="login-form_row">
-              <span className="error">{loginMessage}</span>
+              <h2>Вход:</h2>
             </div>
-          ) : null}
-
-          <div className="login-form_row">
-            <Button
-              className="login"
-              type="submit"
-              disabled={submitting || pristine}
-              tabIndex="3">
-              Войти
-            </Button>
-          </div>
-        </form>
-      )}
-    />
+            <div className="login-form_row">
+              <Field name="username" component="input" validate={required}>
+                {({ input }) => (
+                  <label>
+                    Логин:
+                    <input
+                      {...input}
+                      id="login"
+                      tabIndex="1"
+                      placeholder="логин"
+                    />
+                  </label>
+                )}
+              </Field>
+            </div>
+            <div className="login-form_row">
+              <Field name="password" component="input" validate={required}>
+                {({ input, meta }) => (
+                  <label>
+                    Пароль:
+                    <input
+                      {...input}
+                      id="password"
+                      type="password"
+                      placeholder="пароль"
+                      tabIndex="2"
+                    />
+                  </label>
+                )}
+              </Field>
+            </div>
+            {loginMessage ? (
+              <div className="login-form_row">
+                <span className="error">{loginMessage}</span>
+              </div>
+            ) : null}
+            <div className="login-form_row">
+              <Button
+                className="login"
+                type="submit"
+                disabled={submitting || pristine}
+                tabIndex="3">
+                Войти
+              </Button>
+            </div>
+          </form>
+        )}
+      />
+    </React.Fragment>
   );
 };
 
 LoginForm.propTypes = {
   login: PropTypes.func.isRequired,
   loginMessage: PropTypes.string.isRequired,
+  loginLoading: PropTypes.bool.isRequired,
 };
 
 export default LoginForm;
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 
 import { login } from '../store/actions/user';
-import { getLoginMessage } from '../store/selectors';
+import { getLoginMessage, getLoginLoading } from '../store/selectors';
 
 import LoginForm from '../components/LoginForm';
 
 const mapStateToProps = state => ({
   loginMessage: getLoginMessage(state),
+  loginLoading: getLoginLoading(state),
 });
 
 const mapDispatchToProps = { login };
